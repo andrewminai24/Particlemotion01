0c820995928715d257528dcd83f9f01ed76c11a7
Andrew Minai <andrewminai24@gmail.com>
2019-06-21 04:14:54 -0700

    particlemotion

---
+++ Particle.h
@@ -0,0 +1,31 @@
+/*
+ * Particle.h
+ *
+ * 
+ */
+
+#ifndef PARTICLE_H_
+#define PARTICLE_H_
+
+namespace caveofprogramming {
+
+struct Particle {
+	double m_x;
+	double m_y;
+
+private:
+	double m_speed;
+	double m_direction;
+
+private:
+	void init();
+
+public:
+	Particle();
+	virtual ~Particle();
+	void update(int interval);
+};
+
+} /* namespace caveofprogramming */
+
+#endif /* PARTICLE_H_ */
